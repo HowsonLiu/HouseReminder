cab9260e44bdd686c5310e74bed7493e5918d6b3
HowsonLiu <948470636@qq.com>
2019-05-02 21:51:26 +0800

    house info spider

--- code/WebsiteAnjuke.py
+++ code/WebsiteAnjuke.py
@@ -1,7 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
+
+# 刷新时间
+REFRESH_TIME = 5
 
 # 顺德 最新
 SHUNDE_URL = 'https://foshan.anjuke.com/sale/shundequ/o5/'
+
+HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/71.0.3578.98 Safari/537.36'
+}
 
 class HouseInfo:
     """安居客网站房屋信息结构
@@ -14,5 +23,96 @@ class HouseInfo:
     community = None    # 小区
     position = None     # 位置
 
+class HomeSpider:
+    """安居客首页爬虫
+    """
 
+    url = None
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_first_url(self):
+        """获取下面列表第一个房子的url"""
+        if self.url is None:
+            return None
+        try:
+            html = requests.get(self.url, headers=HEADER)
+            if html.status_code != 200:
+                return None
+        except:
+            return None
+        soup = BeautifulSoup(html.text, 'lxml')
+        house_list = soup.select('#houselist-mod-new > li:nth-child(1) > div.house-details > div.house-title > a')
+        if len(house_list) == 0:
+            return
+        href = house_list[0].get("href")
+        print(href)
+
+class DetailSpider:
+    """安居客详情页爬虫
+    """
+    url = None
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_houseinfo(self):
+        if self.url is None:
+            return
+        try:
+            html = requests.get(self.url, headers=HEADER)
+            if html.status_code != 200:
+                return
+        except:
+            return
+        soup = BeautifulSoup(html.text, 'lxml')
+        info = HouseInfo()
+        # url
+        info.url = self.url
+        # price
+        try:
+            info.price = soup.select('#content > div.wrapper > div.wrapper-lf > div.clearfix > '
+                                     'div.basic-info.clearfix > span.light.info-tag > em')[0].string
+            info.price = info.price + "万"
+        except:
+            info.price = "?万"
+        # perprice
+        try:
+            info.perprice = soup.select('#content > div.wrapper > div.wrapper-lf > div.houseInfoBox > div > '
+                                        'div.houseInfo-wrap > ul > li:nth-child(3) > div.houseInfo-content')[0].string
+        except:
+            info.perprice = "?元/m2"
+        # area
+        try:
+            info.area = soup.select('#content > div.wrapper > div.wrapper-lf > div.houseInfoBox > div > '
+                                    'div.houseInfo-wrap > ul > li:nth-child(5) > div.houseInfo-content')[0].string
+        except:
+            info.area = "?平方米"
+        # pattern
+        try:
+            info.pattern = soup.select('#content > div.wrapper > div.wrapper-lf > div.houseInfoBox > div > '
+                                       'div.houseInfo-wrap > ul > li:nth-child(2) > div.houseInfo-content')[0].string
+            info.pattern = info.pattern.replace('\n', '')
+            info.pattern = info.pattern.replace('\t', '')
+        except:
+            info.pattern = "?室?厅?卫"
+        # community
+        try:
+            info.community = soup.select('#content > div.wrapper > div.wrapper-lf > div.houseInfoBox > div > '
+                                         'div.houseInfo-wrap > ul > li:nth-child(1) > div.houseInfo-content > '
+                                         'a')[0].string
+        except:
+            info.community = "?"
+        # position
+        try:
+            info.position = soup.select('#content > div.wrapper > div.wrapper-lf > div.houseInfoBox > '
+                                                        'div > div.houseInfo-wrap > ul > li:nth-child(4) > '
+                                                        'div.houseInfo-content > p')[0].get_text()
+            info.position = info.position.replace(' ', '')
+            info.position = info.position.replace('\n', '')
+            info.position = info.position.replace('－', ' ')
+        except:
+            info.position = "?"
+        return info
 
