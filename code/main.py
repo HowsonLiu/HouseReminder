55feabd83f3972e898518b3f7e8f6696c19bd48b
HowsonLiu <948470636@qq.com>
2019-05-05 17:20:50 +0800

    smtp will disconnect when it passes a long time

--- code/main.py
+++ code/main.py
@@ -5,33 +5,35 @@ import schedule
 import configparser
 import time
 
-REFERSH_INTERVAL = 30
+REFERSH_INTERVAL = 60
 CONFIG_FILE_PATH = "./setup.cfg"
+ACCOUNT = None
+PASSWORD = None
+OLD_URL = None
 
 def check_and_send():
-    global home_spider, old_url, sender
+    global home_spider, OLD_URL, ACCOUNT, PASSWORD
     lastest_url = home_spider.get_first_url()
     print(lastest_url)
-    if old_url is not None and lastest_url is not None and lastest_url[:-20] == old_url[:-20]:      # 除去url中的时间干扰
+    if OLD_URL is not None and lastest_url is not None and lastest_url[:-20] == OLD_URL[:-20]:      # 除去url中的时间干扰
         return False
     detail_spider = DetailSpider(lastest_url)
     houseinfo = detail_spider.get_houseinfo()
+    sender = EmailSender()
+    sender.login(ACCOUNT, PASSWORD)
     sender.send_house_info(houseinfo)
-    old_url = lastest_url
+    OLD_URL = lastest_url
     return True
 
 def load_configure():
+    global ACCOUNT, PASSWORD
     conf = configparser.ConfigParser()
     conf.read(CONFIG_FILE_PATH, encoding='utf-8')
-    account = conf.get("sender", "account")
-    password = conf.get("sender", "password")
-    return account, password
+    ACCOUNT = conf.get("sender", "account")
+    PASSWORD = conf.get("sender", "password")
 
-old_url = None
-account, password = load_configure()
+load_configure()
 home_spider = HomeSpider(MEIDI_URL)
-sender = EmailSender()
-sender.login(account, password)
 schedule.every(REFERSH_INTERVAL).seconds.do(check_and_send)
 while True:
     schedule.run_pending()
